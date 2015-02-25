1325a4097866791c0518bc4663fafb569c40d534
James Bjorkman <james@bjorkman.ca>
2015-02-25 14:34:02 +1100

    v1.2.0

--- open/index.js
+++ open/index.js
@@ -2,8 +2,16 @@ import { Promise } from "es6-promise";
 
 export default function(key) {
   return new Promise(function(resolve, reject) {
-    if (!localStorage) reject({msg: "No Local Storage"});
+    if (!localStorage) reject({msg: "No localStorage"});
     let value = localStorage.getItem(`ORO::${key}`);
     return resolve(JSON.parse(value));
   });
+};
+
+export function blockingOpen(key) {
+  if (localStorage && localStorage.getItem) {
+    let _value = localStorage.getItem(`ORO::${key}`);
+    return JSON.parse(_value);
+  }
+  return
 };
